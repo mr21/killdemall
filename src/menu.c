866a0beb36a440520577a057e7dec311b59ae82c
Thomas Tortorini <thomastortorini@gmail.com>
2012-09-06 03:36:33 +0200

    Menu: bouton Fullscreen

--- src/menu.c
+++ src/menu.c
@@ -1,3 +1,4 @@
+#include	"SDLazy.h"
 #include	"menu.h"
 #include	"data.h"
 #include	"controls.h"
@@ -21,19 +22,27 @@ void		menu_init(Menu* d)
 
   d->status = 0;
 
-  d->srf[SRF_MENU_BG]       = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(370, 150, 150,  8));
-  d->srf[SRF_MENU_KEYBOARD] = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect( 20, 150, 128, 36));
-  d->srf[SRF_MENU_OPEN]     = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(290, 150,  32, 32));
+  d->srf[SRF_MENU_BG]         = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(370, 150, 150,  8));
+  d->srf[SRF_MENU_FULLSCREEN] = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(370, 185,  30, 27));
+  d->srf[SRF_MENU_KEYBOARD]   = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect( 20, 150, 128, 36));
+  d->srf[SRF_MENU_OPEN]       = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(290, 150,  32, 32));
 
   SDLazy_SetCenterAlign(d->srf[SRF_MENU_BG], ALIGN_TOP | ALIGN_LEFT);
   SDLazy_SetPosAlign(d->srf[SRF_MENU_BG], ALIGN_TOP | ALIGN_RIGHT);
   SDLazy_SetCenterX(d->srf[SRF_MENU_BG], 15);
 
+  b = SDLazy_ButtonCreate(d->srf[SRF_MENU_FULLSCREEN], 2);
+  d->btn[BTN_MENU_FULLSCREEN] = b;
+  SDLazy_SetPosAlign(b, ALIGN_TOP | ALIGN_RIGHT);
+  SDLazy_SetPosY(b, 5);
+  SDLazy_SetCenterAlign(b, ALIGN_TOP | ALIGN_LEFT);
+  SDLazy_ButtonSetFun(b, BUTTON_CLICUP, SDLazy_Fullscreen);
+
   b = SDLazy_ButtonCreate(d->srf[SRF_MENU_KEYBOARD], 2);
   d->btn[BTN_MENU_KEYBOARD] = b;
   SDLazy_SetCenterAlign(b, ALIGN_TOP | ALIGN_LEFT);
   SDLazy_SetPosAlign(b, ALIGN_TOP | ALIGN_RIGHT);
-  SDLazy_SetPosY(b, 20);
+  SDLazy_SetPosY(b, 50);
   SDLazy_ButtonSetFun(b, BUTTON_CLICUP, ctrls_switch);
 
   b = SDLazy_ButtonCreate(d->srf[SRF_MENU_OPEN], 2);
@@ -50,6 +59,9 @@ void		menu_blit(Menu* d)
 
   SDLazy_SetScaleY(d->srf[SRF_MENU_BG], h2 / 4);
   SDLazy_Blit(d->srf[SRF_MENU_BG]);
+
+  SDLazy_SetPosX(d->btn[BTN_MENU_FULLSCREEN], x + 95);
+  SDLazy_Blit(d->btn[BTN_MENU_FULLSCREEN]);
 
   SDLazy_SetPosX(d->btn[BTN_MENU_KEYBOARD], x - 2);
   SDLazy_Blit(d->btn[BTN_MENU_KEYBOARD]);
--- src/render.c
+++ src/render.c
@@ -6,13 +6,17 @@
 
 void		render(void)
 {
+  static Uint32	bg_col;
   Data*		d = SDLazy_GetData();
   v2f		v;
+
+  if (!bg_col)
+    bg_col = SDL_MapRGB(SDLazy_GetScreen()->format, 15, 15, 21);
 
   pos_onscreen(&v, &d->sdldata.cam, v2f_(SDLazy_GetWidth(d->sdldata.srf_bg[BG0]) / -2,
 					 SDLazy_GetHeight(d->sdldata.srf_bg[BG0]) / -2));
 
-  SDL_FillRect(SDLazy_GetScreen(), 0, 0);
+  SDL_FillRect(SDLazy_GetScreen(), 0, bg_col);
 
   SDLazy_SetPos(d->sdldata.srf_bg[BG0], &v);
   SDLazy_Blit(d->sdldata.srf_bg[BG0]);
