380017c397a272cc6252f86a2dad3b27d6a46da2
Thomas Tortorini <thomastortorini@gmail.com>
2012-09-07 23:50:49 +0200

    Ajout dun nouveau bouton

--- src/menu.c
+++ src/menu.c
@@ -25,6 +25,7 @@ void		menu_init(Menu* d)
   d->srf[SRF_MENU_BG]         = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(370, 150, 150,  8));
   d->srf[SRF_MENU_FULLSCREEN] = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(370, 185,  30, 27));
   d->srf[SRF_MENU_KEYBOARD]   = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect( 20, 150, 128, 36));
+  d->srf[SRF_MENU_COMMANDS]   = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect( 20, 150, 128, 36));
   d->srf[SRF_MENU_OPEN]       = SDLazy_SurfaceLoad("img/sprites.png", SDLazy_Rect(290, 150,  32, 32));
 
   SDLazy_SetCenterAlign(d->srf[SRF_MENU_BG], ALIGN_TOP | ALIGN_LEFT);
@@ -44,6 +45,13 @@ void		menu_init(Menu* d)
   SDLazy_SetPosAlign(b, ALIGN_TOP | ALIGN_RIGHT);
   SDLazy_SetPosY(b, 50);
   SDLazy_ButtonSetFun(b, BUTTON_CLICUP, ctrls_qwerty_azerty);
+
+  b = SDLazy_ButtonCreate(d->srf[SRF_MENU_COMMANDS], 2);
+  d->btn[BTN_MENU_COMMANDS] = b;
+  SDLazy_SetCenterAlign(b, ALIGN_TOP | ALIGN_LEFT);
+  SDLazy_SetPosAlign(b, ALIGN_TOP | ALIGN_RIGHT);
+  SDLazy_SetPosY(b, 100);
+  SDLazy_ButtonSetFun(b, BUTTON_CLICUP, ctrls_switch_cmds);
 
   b = SDLazy_ButtonCreate(d->srf[SRF_MENU_OPEN], 2);
   d->btn[BTN_MENU_OPEN] = b;
@@ -65,6 +73,9 @@ void		menu_blit(Menu* d)
 
   SDLazy_SetPosX(d->btn[BTN_MENU_KEYBOARD], x - 2);
   SDLazy_Blit(d->btn[BTN_MENU_KEYBOARD]);
+
+  SDLazy_SetPosX(d->btn[BTN_MENU_COMMANDS], x - 2);
+  SDLazy_Blit(d->btn[BTN_MENU_COMMANDS]);
 
   SDLazy_SetPos(d->btn[BTN_MENU_OPEN], v2f_(x - 6, h2));
   SDLazy_Blit(d->btn[BTN_MENU_OPEN]);
