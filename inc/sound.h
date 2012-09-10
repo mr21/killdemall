abff151ae8153266542b799acd23153cb22da1ef
Thomas Tortorini <thomastortorini@gmail.com>
2012-09-10 22:52:27 +0200

    Quelques fonctions pour FmodEx

--- inc/sound.h
+++ inc/sound.h
@@ -4,11 +4,20 @@
 #include	"fmod.h"
 #include	"fmod_errors.h"
 
+typedef		enum
+{
+  SND_XHSIP_SHOOT,
+  NB_SND
+}		eSound;
+
 typedef		struct
 {
   FMOD_SYSTEM*	system;
+  FMOD_SOUND*	mp3[NB_SND];
+  FMOD_CHANNEL*	chan;
 }		Sound;
 
 int		sound_init(Sound*);
+void		sound_play(eSound, float vol, float pan, float freq);
 
 #endif
