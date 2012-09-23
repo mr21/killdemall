4a0268b8ea43ea6ed0ab6f7accc5b95320a1a963
Mr21 <thomastortorini@gmail.com>
2012-09-23 05:36:42 +0200

    Patch d'une vilaine division par 0

--- src/xship_core.c
+++ src/xship_core.c
@@ -3,11 +3,11 @@
 void		xship_core(XShip* s)
 {
   v2f		v = *SDLazy_GetPos(s->cannon_anim);
-  double	d = 0;
+  double	d;
 
   v.x = g_sdlazy.mouse.x - v.x;
   v.y = g_sdlazy.mouse.y - v.y;
-  d = -atan(v.x / v.y);
+  d = v.y ? -atan(v.x / v.y) : 0;
   if (v.y >= 0)
     d += M_PI;
   SDLazy_SetRot(s->cannon_anim, d);
