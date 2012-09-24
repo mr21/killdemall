f51ab9d569605908248c53dc4e8b1312be306b2b
Thomas Tortorini <thomastortorini@gmail.com>
2012-09-24 17:53:29 +0200

    Recoil sur le canon primaire

--- src/xship_cannon_fire.c
+++ src/xship_cannon_fire.c
@@ -15,9 +15,11 @@ void		xship_cannon_fire(XShip* s)
   b.dir.y = cos(radAn - M_PI);
   b.pos.x = s->ship.pos.x + b.dir.x * 40;
   b.pos.y = s->ship.pos.y + b.dir.y * 40;
+  ship_recoil((Ship*)s, -b.dir.x * 200, SHIP_AXE_X);
+  ship_recoil((Ship*)s, -b.dir.y * 200, SHIP_AXE_Y);
   b.dir.x *= 700.;
   b.dir.y *= 700.;
   ammo_push(SDLazy_GetData(), &b);
-  sound_play(SND_XSHIP_CANNON, .8, 0, 44100);
+  sound_play(SND_XSHIP_CANNON, .8, 0, 30000 + rand() % 15000);
   SDLazy_AnimReplay(s->cannon_anim);
 }
