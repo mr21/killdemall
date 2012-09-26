dcc545420492b8d69116db5967771aa25628430d
Thomas Tortorini <thomastortorini@gmail.com>
2012-09-26 22:04:22 +0200

    Menu openclose

--- src/menu/menu_openclose.c
+++ src/menu/menu_openclose.c
@@ -2,20 +2,20 @@
 #include	"sound.h"
 #include	"data.h"
 
-static void	snd_open_close(int status)
+static void	_snd(int status)
 {
   sound_play(SND_MENU_OPEN, .9, .8, status ? 25000 : 22050);
 }
 
 void		menu_open(Menu* d)
 {
-  snd_open_close(d->status = 1);
+  _snd(d->status = 1);
   SDLazy_ButtonSetState(d->btn[BTN_MENU_OPEN], 1);
 }
 
 void		menu_close(Menu* d)
 {
-  snd_open_close(d->status = 0);
+  _snd(d->status = 0);
   SDLazy_ButtonSetState(d->btn[BTN_MENU_OPEN], 0);
 }
 
@@ -23,5 +23,5 @@ void		menu_toggle(void)
 {
   Menu*         d = &((Data*)SDLazy_GetData())->menu;
 
-  snd_open_close(d->status = !d->status);
+  _snd(d->status = !d->status);
 }
