206dd256d5700ec3376c9e8c6088f7f2757cea85
Mr21 <thomastortorini@gmail.com>
2012-10-07 23:26:09 +0200

    Le cannon du XShip se rotate en fonction de la souris uniquement si cest le notre

--- src/ship/xship/xship_core_rot_cannon.c
+++ src/ship/xship/xship_core_rot_cannon.c
@@ -1,19 +1,24 @@
 #include	"xship.h"
+#include	"data.h"
 
-void		xship_core_rot_cannon(XShip* s)
+void		xship_core_rot_cannon(XShip* x)
 {
-  v2f		v = *SDLazy_GetPos(s->cannon_anim);
-  double	d;
+  v2f		v;
+  double	rad;
 
-  v.x = SDLazy_GetMouseX() - v.x;
-  v.y = SDLazy_GetMouseY() - v.y;
-  if (!v.y)
-    d = v.x > 0 ? M_PI2 : -M_PI2;
-  else
+  if ((Ship*)x == ((Data*)SDLazy_GetData())->player)
     {
-      d = -atan(v.x / v.y);
-      if (v.y > 0)
-	d += M_PI;
+      v = *SDLazy_GetPos(x->cannon_anim);
+      v.x = SDLazy_GetMouseX() - v.x;
+      v.y = SDLazy_GetMouseY() - v.y;
+      if (!v.y)
+	rad = v.x > 0 ? M_PI2 : -M_PI2;
+      else
+	{
+	  rad = -atan(v.x / v.y);
+	  if (v.y > 0)
+	    rad += M_PI;
+	}
+      SDLazy_SetRot(x->cannon_anim, rad);
     }
-  SDLazy_SetRot(s->cannon_anim, d);
 }
