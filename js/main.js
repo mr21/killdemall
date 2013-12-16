ac5c88c5a3020efb8ed62bb331ebbef7851e8019
Mr21 <thomastortorini@gmail.com>
2013-12-16 02:53:13 -0500

    JS, boucle de jeu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 function lg(s) { console.log(s) }
 
+var killdemall = {
+	init: function() {
+		var self     = this;
+		this.ctx     = document.getElementsByTagName('canvas')[0].getContext('2d');
+		this.png     = new Image();
+		this.png.src = 'css/sprites.png';
+		this.png.onload = function() {
+			self.assetsManager = new AssetsManager(self.ctx, this);
+			self.xship_01 = new self.XShip();
+			self.intervId = window.setInterval(function() {
+				self.loop();
+			}, 1000/40);
+		};
+	},
+	loop: function() {
+		this.render();
+	},
+	render: function() {
+		var ctx = this.ctx;
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+		
+		ctx.save();
+			ctx.translate(100, 100);
+				this.xship_01.render();
+		ctx.restore();
+	}
+};
+
 window.onload = function() {
-	lg('main()');
+	killdemall.init();
 };
