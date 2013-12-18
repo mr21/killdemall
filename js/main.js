c55a7093908562a0c08f647f39abd19cc6327bf1
Mr21 <thomastortorini@gmail.com>
2013-12-18 13:49:55 -0500

    JS, mise en place du TimeManager et AssetsManager presque termine

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ var killdemall = {
 		this.ctx     = document.getElementsByTagName('canvas')[0].getContext('2d');
 		this.png     = new Image();
 		this.png.src = 'css/sprites.png';
+		this.timeManager = new TimeManager();
 		this.png.onload = function() {
-			self.assetsManager = new AssetsManager(self.ctx, this);
+			self.assetsManager = new AssetsManager(self.ctx, self.timeManager, this);
 			self.xship_01 = new self.XShip();
 			self.intervId = window.setInterval(function() {
 				self.loop();
@@ -15,12 +16,13 @@ var killdemall = {
 		};
 	},
 	loop: function() {
+		this.timeManager.update();
 		this.render();
+		this.assetsManager.update();
 	},
 	render: function() {
 		var ctx = this.ctx;
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-		
 		ctx.save();
 			ctx.translate(100, 100);
 				this.xship_01.render();
