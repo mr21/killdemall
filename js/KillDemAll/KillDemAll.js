30788bf670f8f2e00df2e3359cc62f761e938bcc
Mr21 <thomastortorini@gmail.com>
2013-12-19 02:28:49 -0500

    JS, Canvas2D, gestion de la souris

--- js/KillDemAll/KillDemAll.js
+++ js/KillDemAll/KillDemAll.js
@@ -5,10 +5,13 @@ var KillDemAll = {
 			elem,
 			['css/sprites.png'],
 			{
-				load    : function()  { self.load()     },
-				loop    : function()  { self.loop()     },
-				keydown : function(e) { self.keydown(e) },
-				keyup   : function(e) { self.keyup(e)   }
+				load      : function()  { self.load()     },
+				loop      : function()  { self.loop()     },
+				keydown   : function(e) { self.keydown(e) },
+				keyup     : function(e) { self.keyup(e)   },
+				mousedown : function()  { self.mousedown.apply(self, arguments) },
+				mouseup   : function()  { self.mouseup  .apply(self, arguments) },
+				mousemove : function()  { self.mousemove.apply(self, arguments) }
 			}
 		);
 	},
@@ -16,10 +19,19 @@ var KillDemAll = {
 		this.xship_01 = new KillDemAll.XShip();
 	},
 	keydown: function(key) {
-		lg('keydown :' + key);
+		lg('keydown(' + key + ')');
 	},
 	keyup: function(key) {
-		lg('keyup   :' + key);
+		lg('keyup(' + key + ')');
+	},
+	mousedown: function(x, y) {
+		lg('mousedown(' + x + ', ' + y + ')');
+	},
+	mouseup: function(x, y) {
+		lg('mouseup(' + x + ', ' + y + ')');
+	},
+	mousemove: function(x, y, xRel, yRel) {
+		lg('mousemove(' + x + ', ' + y + ', ' + xRel + ', ' + yRel + ')');
 	},
 	loop: function() {
 		var ctx = this.canvas2d.ctx;
