d8cf31a4290ccd7f9cd341777f76b6d49d4df502
Mr21 <thomastortorini@gmail.com>
2013-12-19 17:45:58 -0500

    JS, Canvas2D, Vector.js

---
+++ js/Canvas2D/Vector.js
@@ -0,0 +1,29 @@
+function Vector2D(x, y) {
+	this.set(x, y);
+}
+Vector2D.prototype = {
+	set: function(x, y) {
+		this.x = x;
+		this.y = y;
+	},
+	add: function(v, y) {
+		this.x += v.x || v;
+		this.y += v.y || y;
+	},
+	norm: function() {
+		return Math.sqrt(
+			this.x * this.x +
+			this.y * this.y
+		);
+	},
+	normalize: function() {
+		var n = this.norm();
+		if (n) {
+			this.x /= n;
+			this.y /= n;
+		}
+	},
+	show: function() {
+		console.log('vector = {x:' + this.x + ', y:' + this.y + '}');
+	}
+};
