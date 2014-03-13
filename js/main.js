354755e792a724e6a884fbdf51c4e402194bfd14
Mr21 <thomastortorini@gmail.com>
2014-03-13 10:57:32 -0400

    PNG/JS/HTML/CSS, page d'accueil, plusieurs autres boutons, etc

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ function lg(s) { console.log(s) }
 
 window.onload = function()
 {
+	var container = document._domSelector('.canvasloth')[0];
 	var canvasloth = new Canvasloth({
 		type   : '2d',
-		node   : document._domSelector('.canvasloth')[0],
+		node   : container,
 		app    : KillDemAll,
 		images : [
 			'css/explosion.png',
@@ -25,15 +26,23 @@ window.onload = function()
 		}
 	});
 
-	var domA_menu = document.getElementById('menu').getElementsByTagName('a');
+	var domA_menu = document._domSelector('#menu a');
 	for (var i = 0, a; a = domA_menu[i]; ++i)
-		a.onclick = function() { return canvasloth.pages.open(document._domSelector('.canvasloth-page.' + this.className)[0]), false };
+		a.onclick = function() {
+			canvasloth.pages.open(container._domSelector('.canvasloth-page.' + this.className)[0]);
+			return false;
+		};
 
-	document._domSelector('.canvasloth-page.gameover a')[0].onclick = function() {
+	container._domSelector('.start    a.btn.start')[0].onclick =
+	container._domSelector('.gameover a.btn.retry')[0].onclick = function() {
 		KillDemAll.reset();
-		KillDemAll.canvasloth.pages.close();
-		KillDemAll.canvasloth.focus();
+		canvasloth.pages.close();
+		canvasloth.focus();
 		return false;
 	};
 
+	container._domSelector('.gameover a.btn.quit')[0].onclick = function() {
+		canvasloth.pages.open();
+		return false;
+	};
 };
